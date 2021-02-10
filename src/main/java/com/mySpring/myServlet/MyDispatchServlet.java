f275372389e2e8e89ee7d226f7eaa927905a9b91
DOCCA0 <2947323341@qq.com>
2021-02-10 14:37:09 +0800

    初步完成ioc

--- src/main/java/com/mySpring/myServlet/MyDispatchServlet.java
+++ src/main/java/com/mySpring/myServlet/MyDispatchServlet.java
@@ -1,5 +1,9 @@
 package com.mySpring.myServlet;
 
+import com.myDemo.Student;
+import com.mySpring.myAnnotations.MyAutowired;
+import com.mySpring.myAnnotations.MyController;
+import com.mySpring.myAnnotations.MyService;
 import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
@@ -7,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.util.*;
 
 /**
@@ -32,15 +37,20 @@ public class MyDispatchServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        //1、load the properties file
+        //1,load the properties file
         doLoadConfig("application.properties");
+        //2,scan the package,for example:com.myDemo
         doScanner(properties.getProperty("scan-package"));
+        //3,put beans into ioc container
+        doInstance();
+        doAutowired();
     }
 
 
 
+
     /**
-     * load properties file into RAM,and we use MyDispatchServlet.properties to store them
+     * 1,load properties file into RAM,and we use MyDispatchServlet.properties to store them
      * @param contextConfigLocation file path of the properties file
      */
     private void doLoadConfig(String contextConfigLocation) {
@@ -63,10 +73,68 @@ public class MyDispatchServlet extends HttpServlet {
         }
     }
 
+    /**
+     * 2,scan all the ".java" file in dir "path"
+     * @param path package path
+     */
     private void doScanner(String path) {
-        System.out.printf(path);
+        File dirPath = new File("src/main/java/"+path.replace('.','/'));
+        if (dirPath==null)
+            return;
+        for (File file:dirPath.listFiles()){
+            if (file.isDirectory()){
+                doScanner(file.getPath().replace("src/main/java/",""));
+            }else{
+                if (file.getAbsolutePath().endsWith(".java")){
+                    String beanName=file.getPath().replace("src/main/java/","")
+                            .replace("/",".").replace(".java","");
+                    beanNameList.add(beanName);
+                    log.info("beanListName add:"+beanName);
+                }
+            }
+        }
     }
-    //2、扫描相关类
-    //3、将相关类保存在iocMap里
     //4、将iocMap里的类依赖注入
+
+    /**
+     * 3,put beans into ioc container
+     */
+    private void doInstance()  {
+        for (String beanName:beanNameList){
+            Class<?> aClass = null;
+            try {
+                aClass = Class.forName(beanName);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            if (aClass.isAnnotationPresent(MyController.class)){
+                Object o = null;
+                try {
+                    o = aClass.newInstance();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                log.info("put "+beanName+" into container");
+                iocMap.put(beanName,o);
+            }else if(aClass.isAnnotationPresent(MyService.class)){
+
+
+            }
+
+        }
+    }
+
+    private void doAutowired() {
+        for (Map.Entry<String,Object> entry: iocMap.entrySet()){
+            Field[] fields = entry.getValue().getClass().getDeclaredFields();
+            for (Field field:fields){
+                if(!field.isAnnotationPresent(MyAutowired.class)){
+                    continue;
+                }
+
+            }
+        }
+    }
 }
